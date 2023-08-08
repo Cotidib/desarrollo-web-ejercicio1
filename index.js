92fae9abec7d987aa5655a4a4458e3e9e09127d9
Cotidib <constanza.dibueno@gmail.com>
2023-08-07 21:00:08 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function funcionSorpresa(){
+  document.getElementById("atun").classList.toggle("hide");
+  document.getElementById("right-heart").classList.toggle("hide");
+  document.getElementById("left-heart").classList.toggle("hide");
+}
